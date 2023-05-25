0774c766111231263eb48a64de6c20024e181179
joao-ar11 <jarlpereira29@gmail.com>
2023-05-25 15:08:36 -0300

    Consertar carousel e apagar secao pdschool

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,24 @@
-// Fazendo com que o segundo ítem fique no meio do carousel
-document.querySelectorAll('.depoimento')[2].scrollIntoView({
-  inline: 'center',
-  behavior: 'smooth',
-});
-
 // Index do item do meio atual
 let currentIndex = 2;
 
 // Evento dos botões do carousel
 document.querySelectorAll('.carousel-button').forEach(e => e.addEventListener('click', () => {
-
   // Se for o da esquerda diminui o valor, se não, aumenta
   currentIndex += e.id === 'anterior'? -1 : 1;
   const listaDeCards = document.querySelectorAll('.depoimento');
-
+  
   // Garantindo que o index não saia do range válido
-  // OBS: card 0 e -1 são vazios e só existem para que o primeiro e último possam ficar no meio
-  if (currentIndex < 1) {
+  // OBS: sempre haverão ter três cards na tela, ou seja, o 1o e o último nunca estrão no meio
+  if (currentIndex < 2) {
     currentIndex = listaDeCards.length - 2;
   } else if (currentIndex > listaDeCards.length - 2) {
     currentIndex = 1;
   }
-
+  console.log(currentIndex)
   // Movendo os cards para que o card do index esteja no meio
   listaDeCards[currentIndex].scrollIntoView({
     inline: 'center',
+    block: 'nearest',
     behavior: 'smooth',
   });
 }));
